02ec4e337a0c273b0bf3e710db2cbc539a75b2f7
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2025-03-31 18:35:32 -0400

    All models working and Django Administration is working as it was expected now. The next step is to build the Website form and design the Web Page to allow the user to introduce the information accessing to a Website

--- SimpleBill/simpleBilling/admin.py
+++ SimpleBill/simpleBilling/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client, Invoice, Attachment
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Invoice)
+admin.site.register(Attachment)
