013816b6fccaeda9955e20e5b46509d875a0de5e
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2025-04-09 22:15:17 -0400

    Creating a new file after processing the button submit invoice

--- SimpleBill/simpleBilling/urls.py
+++ SimpleBill/simpleBilling/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
      path('invoice/create/', views.create_invoice_view, name='create-invoice'),
+     path('invoice/success/', views.invoice_success_view, name='invoice-success'),
 ]
--- SimpleBill/simpleBilling/views.py
+++ SimpleBill/simpleBilling/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Client, Invoice, Attachment
-
-# Create your views here.
-def create_invoice_view(request):
-    return render(request, 'simpleBilling/invoice.html')
+from decimal import Decimal
 
 def create_invoice_view(request):
     if request.method == 'POST':
         client_name = request.POST.get('client_name')
-        service_fee = request.POST.get('service_fee')
-        travel_expenses = request.POST.get('travel_expenses')
-        tax_percent = request.POST.get('tax_percent')
+        service_fee = Decimal(request.POST.get('service_fee'))
+        travel_expenses = Decimal(request.POST.get('travel_expenses'))
+        tax_percent = Decimal(request.POST.get('tax_percent'))
         uploaded_files = request.FILES.getlist('attachments')
         
-        client,
+        client, _ = Client.objects.get_or_create(name=client_name)
         
+        invoice = Invoice.objects.create(
+            client=client,
+            service_fee=service_fee,
+            travel_expenses=travel_expenses,
+            tax_percent=tax_percent,
+        )
+        
+        for file in uploaded_files:
+            Attachment.objects.create(invoice=invoice, file=file)
+            
+        return redirect('invoice-success')
+    
+    return render(request, 'simpleBilling/invoice.html')
+    
+        
+def invoice_success_view(request):
+    return render(request, 'simpleBilling/success.html')
