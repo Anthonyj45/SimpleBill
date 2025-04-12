021f17344876607202bab5485e87316db69953f0
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2025-04-11 20:27:42 -0400

    Adding some features to allow the use of ngrok link to access to the website

--- SimpleBill/SimpleBill/settings.py
+++ SimpleBill/SimpleBill/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = 'django-insecure-wnslhdu4yz^_)11@y7w)@k&d-paw*$3zi0l&wwk(cg3l)y7m&h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '094e-172-97-132-80.ngrok-free.app']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://094e-172-97-132-80.ngrok-free.app',
+]
 
 # Application definition
 
